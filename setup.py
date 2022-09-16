c18a90fe6a1a29943d39931afa3634398c115c31
Leonard Papenmeier <leonard.papenmeier@posteo.de>
2022-09-16 11:31:26 +0200

    fix versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="baxus",
-    version=open("VERSION.md").readline().split(" ")[2],
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.21",
